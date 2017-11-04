aefd377b982004bc5080aab911f4e84b5ddfee96
wflohry <wflohry@gmail.com>
2017-11-03 22:15:02 -0500

    Updated imgui

--- include/CinderImGui.h
+++ include/CinderImGui.h
@@ -64,6 +64,7 @@ operator ci::Color() const { return ci::Color(x,y,z); }
 
 #include "CinderImGuiExports.h"
 #include "imgui.h"
+#include "imguidock.h"
 
 #ifndef CINDER_IMGUI_NO_NAMESPACE_ALIAS
 namespace ui = ImGui;
@@ -306,22 +307,6 @@ namespace ImGui {
 			ui::SetCurrentContext( ctx );
 		}
 	}
-
-	// Dock From LumixEngine
-	// https://github.com/nem0/LumixEngine/blob/master/external/imgui/imgui_dock.h
-	// https://github.com/ocornut/imgui/issues/351
-	// #define IMGUI_DOCK
-#if defined( IMGUI_DOCK )
-	IMGUI_API void ShutdownDock();
-	IMGUI_API void RootDock(const ImVec2& pos, const ImVec2& size);
-	IMGUI_API bool BeginDock(const char* label, bool* opened = nullptr, ImGuiWindowFlags extra_flags = 0, const ImVec2& default_size = { 0, 0 });
-	IMGUI_API void EndDock();
-	IMGUI_API void SetDockActive();
-	IMGUI_API bool isMouseHoveringDock();
-	IMGUI_API std::string toStringJson();
-	IMGUI_API void fromStringJson(const std::string &s);
-	IMGUI_API bool isDockActive();
-#endif
 
 	// Getters/Setters Helpers Implementation
 	template<typename T>
